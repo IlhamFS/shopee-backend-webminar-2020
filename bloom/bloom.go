07d902546c4ff1af63d82a63c14fb2378be7989f
Ilham <ilham.fathy@shopee.com>
2020-09-27 17:16:53 +0700

    change to int bloom

--- bloom/bloom.go
+++ bloom/bloom.go
@@ -10,7 +10,7 @@ import (
 // BloomFilter probabilistic data structure definition
 type BloomFilter struct {
 	// Please use math/big as an alternative for bitset
-	bitset  []bool        // The bloom-filter bitset
+	bitset  []int         // The bloom-filter bitset
 	n       uint          // Number of elements in the filter
 	m       uint          // Size of the bloom filter
 	hashfns []hash.Hash64 // The hash functions
@@ -23,14 +23,14 @@ var DefaultHashFunctions = []hash.Hash64{murmur3.New64(), fnv.New64(), fnv.New64
 func NewBloomFilter(m uint, hashes []hash.Hash64) *BloomFilter {
 	if hashes == nil || len(hashes) <= 0 {
 		return &BloomFilter{
-			bitset:  make([]bool, m),
+			bitset:  make([]int, m),
 			m:       m,
 			n:       uint(0),
 			hashfns: DefaultHashFunctions,
 		}
 	}
 	return &BloomFilter{
-		bitset:  make([]bool, m),
+		bitset:  make([]int, m),
 		m:       m,
 		n:       uint(0),
 		hashfns: hashes,
@@ -40,9 +40,12 @@ func NewBloomFilter(m uint, hashes []hash.Hash64) *BloomFilter {
 // Adds the item into the bloom filter set by hashing in over the hash functions
 func (bf *BloomFilter) Add(item []byte) {
 	for _, v := range bf.convertToHashes(item) {
+		// hash result example = "131313123123131233123123" want to fit in the vector bit? Use modulo
 		position := uint(v) % bf.m
-		bf.bitset[position] = true
+		// position must be between 0 and (m - 1)
+		bf.bitset[position] = 1
 	}
+	// ex: hash results after module: 0, 2, 3 ->> [1, 0, 1, 1]
 	bf.n += 1
 }
 
@@ -50,7 +53,7 @@ func (bf *BloomFilter) Add(item []byte) {
 func (bf *BloomFilter) Test(item []byte) bool {
 	for _, v := range bf.convertToHashes(item) {
 		position := uint(v) % bf.m
-		if !bf.bitset[position] {
+		if bf.bitset[position] != 1 {
 			return false
 		}
 	}
@@ -59,6 +62,7 @@ func (bf *BloomFilter) Test(item []byte) bool {
 
 // Calculates all the hash values by hashing in over the hash functions
 func (bf *BloomFilter) convertToHashes(item []byte) []uint64 {
+	// result length depends on total hash functions
 	var result []uint64
 
 	for _, hashFunc := range bf.hashfns {
